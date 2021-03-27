60e03e7b2ba8e39b5232bf7346abfb3c3a01006a
Hyunhoi Koo <hhoikoo00@gmail.com>
2021-03-27 22:17:40 +0900

    Add VueJS Skeleton

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <p>Hello, world!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
